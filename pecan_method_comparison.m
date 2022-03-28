8c56105f1c67b54ecf2055c59eb30a1a44ca968f
Dani Agramonte <74071056+JosriqueJotaro@users.noreply.github.com>
2022-03-28 19:51:07 -0400

    Create Pecan Comparison Method

---
+++ pecan_method_comparison.m
@@ -0,0 +1,10 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% DDSL - Pecan Project
+% 
+% Compare direct area method vs bounding box scaling method
+%
+% Author: Dani Agramonte
+% Last Updated: 03.38.22
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
