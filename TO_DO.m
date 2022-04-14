3f0fecf5702c7b79a70ae2477893e7944d44f5f5
JosriqueJotaro <74071056+JosriqueJotaro@users.noreply.github.com>
2022-04-14 10:26:56 -0400

    Create TO_DO
    text script with list of features and things to do in project

---
+++ TO_DO.m
@@ -0,0 +1,16 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% DDSL - Pecan Project
+% 
+% 1. finish quality of life improvements for pecan_data_struct_create as
+% shown on the script itself
+% 2. create script which converts between different units
+% 3. add unit info on pecan_data_struct_create - i.e., pass unit info from
+% calibration scripts to data_structs
+% 4. edit calibration scripts to output unit
+% 5. update PHE_method_comp for new versions of code
+% 6. remove trash files/scripts/functions from system
+%
+% Author: Dani Agramonte
+% Last Updated: 04.14.22
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
