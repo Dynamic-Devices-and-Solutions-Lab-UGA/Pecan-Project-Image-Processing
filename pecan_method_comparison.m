e368a6a70d38dd30055438225f8825f951484769
JosriqueJotaro <74071056+JosriqueJotaro@users.noreply.github.com>
2022-04-28 17:47:46 -0400

    finish pecan method comparison

--- pecan_method_comparison.m
+++ pecan_method_comparison.m
@@ -8,71 +8,76 @@
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+
+%% MATLAB initialization
 clear; % Clear variables
 clc;  % Clear command window.
 workspace;  % Make sure the workspace panel is showing.
 
-% The following is for full half comparison
+% set debug parameter
+debug = 'false'; % valid values are 'true' or 'false'
+
+%% Load in data and calculate dates in unix time
 
 % Get pre crack files
 pre_crack_files = dir(fullfile(fullfile(pwd,...
-    'Pecan_Data_03.24.22/Pre_Crack'), '*.jpg'));
+    'Pecan_Calibration_Images/Pre_Crack'), '*.jpg'));
 
-%{
-% The following is for partial half comparison
-pre_crack_files = dir(fullfile(fullfile(pwd,...
-    'Pecan_Data_03.31.22_Test_Partial_Halves/Pre_Crack'), '*.jpg'));
-%}
-
-% number of pecans to consider
-n_pecan_pre_crack = length(pre_crack_files);
+% number of pre crack pecans
+n_pecan_pre_crack = size(pre_crack_files,1);
 
 % initialize double array of pre crack time stamps
 time_stamps_pre_crack = zeros(n_pecan_pre_crack,1);
 
-% get photo timestamps for pre crack files
+% get photo timestamps for pre crack files in unix time
 for i = 1:n_pecan_pre_crack
-    time_stamps_pre_crack(i) = str2double(extractBetween(...
-        pre_crack_files(i).name,'_','.'));
+    time_stamps_pre_crack(i) = time_unix(pre_crack_files(i).name);
 end
 
-
-% The following is for full half comparison
-
-%{
 % Get post crack files
 post_crack_files = dir(fullfile(fullfile(pwd,...
-    'Pecan_Data_03.24.22/Post_Crack'), '*.jpg'));
-post_crack_files = [post_crack_files; dir(fullfile(fullfile(pwd,...
-    'Pecan_Data_03.31.22_Test_Full_Halves/Post_Crack'), '*.jpg'))];
-%}
+    'Pecan_Calibration_Images/Post_Crack'), '*.jpg'));
 
-%{
-% The following is for partial half comparison
-post_crack_files = dir(fullfile(fullfile(pwd,...
-    'Pecan_Data_03.31.22_Test_Partial_Halves/Post_Crack'), '*.jpg'));
-%}
+% number of post crack files; equals number of calibration points
+n_pecan_post_crack = size(post_crack_files,1);
 
-% number of full halves to consider
-n_pecan_post_crack = length(post_crack_files);
-
-% initialize double array of pre crack time stamps
+% initialize double array of post crack time stamps
 time_stamps_post_crack = zeros(n_pecan_post_crack,1);
 
-% get photo timestamps for pre crack files
+% get photo timestamps for post crack files
 for i = 1:n_pecan_post_crack
-    time_stamps_post_crack(i) = str2double(extractBetween(...
-        post_crack_files(i).name,'_','.'));
+    time_stamps_post_crack(i) = time_unix(post_crack_files(i).name);
 end
 
+% Get diseased files
+diseased_files = dir(fullfile(fullfile(pwd,...
+    'Pecan_Calibration_Images/Diseased'), '*.jpg'));
+
+% number of diseased files
+n_pecan_diseased = size(diseased_files,1);
+
+% initialize double array of diseased time stamps
+time_stamps_diseased = zeros(n_pecan_diseased,1);
+
+% get photo timestamps for diseased files
+for i = 1:n_pecan_diseased
+    time_stamps_diseased(i) = time_unix(diseased_files(i).name);
+end
+
+%% Order array
+
 % array of timestamps for all files
-time_stamps_aggregate = [time_stamps_pre_crack;time_stamps_post_crack];
+time_stamps_aggregate = [time_stamps_pre_crack;...
+    time_stamps_post_crack;time_stamps_diseased];
 
 % sorted time stamp array
 [time_stamps_aggregate_sort, I_sort] = sort(time_stamps_aggregate);
 
-% initialize array of comparison
-pecan_method_comp = zeros(n_pecan_post_crack,2);
+% initialize array of calibration data
+%
+% | bounding_box | direct_area | calib_surf |
+%
+pecan_method_comp = zeros(n_pecan_post_crack,3);
 
 % get array of post crack indices
 I_sort_post_crack = zeros(n_pecan_post_crack,1);
@@ -81,8 +86,13 @@ for i = 1:n_pecan_post_crack
     I_sort_post_crack(i) = find(I_sort == (n_pecan_pre_crack+i));
 end
 
+%% Populate PHE Comparison matrix
+
 % use bounding box method
 for i = 1:n_pecan_post_crack
+    % display iteration for debugging purposes
+    disp(i)
+    
     % get post crack index and filename
     post_crack_ind = I_sort(I_sort_post_crack(i))-n_pecan_pre_crack;
     post_crack_file = fullfile(...
@@ -107,36 +117,46 @@ for i = 1:n_pecan_post_crack
         pre_crack_files(pre_crack_ind).folder,...
         pre_crack_files(pre_crack_ind).name);
     
-    [pecan_method_comp(i,1),~,~] = PHE(pre_crack_file,post_crack_file,...
-        'method','bounding_box','pre_cracked_bw','true');
+    [pecan_method_comp(i,1),~,~,~,~,~,~] = PHE(pre_crack_file,post_crack_file,...
+        'method','bounding_box');
+    [pecan_method_comp(i,2),~,~,~,~,~,~] = PHE(pre_crack_file,post_crack_file,...
+        'method','direct_area');
+    [pecan_method_comp(i,3),~,~,~,~,~,~] = PHE(pre_crack_file,post_crack_file,...
+        'method','calib_surf');
 end
 
-% use direct area method
-for i = 1:n_pecan_post_crack
-    % get post crack index and filename
-    post_crack_ind = I_sort(I_sort_post_crack(i))-n_pecan_pre_crack;
-    post_crack_file = fullfile(...
-        post_crack_files(post_crack_ind).folder,...
-        post_crack_files(post_crack_ind).name);
+%% Shutdown tasks
+
+% save data to .mat file
+save('C:\Users\Dani\Documents\Pecan-Project-Image-Processing\Pecan_Calibration_Data\PMC_Data.mat','pecan_method_comp');
+
+% clear data
+clear; % Clear variables
+clc;  % Clear command window.
+
+%-----------END MAIN SCRIPT-----------%
+
+%% get unix time function
+
+function date = time_unix(string)
+% time_unix: get string from picture files and return the time the picture
+% was taken in unix time, i.e., time elapsed since midnight January 1st,
+% 1970.
+
+% auxiliary variable to get first half of date info
+aux = strsplit(string,'_');
+
+% convert to character array
+yyyymmdd = char(aux(1));
+
+% relabel aux variable to get second half of date info
+aux = strsplit(char(aux(2)),'.');
+
+% convert to character array
+hhmmss = char(aux(1));
+date = convertTo(datetime(str2double(yyyymmdd(1:4)),...
+    str2double(yyyymmdd(5:6)),str2double(yyyymmdd(7:8)),...
+    str2double(hhmmss(1:2)),str2double(hhmmss(3:4)),...
+    str2double(hhmmss(5:6))),'posixtime');
     
-    % initialize indexing variable
-    j = 1;
-    
-    % count backwards until a pre crack index is found
-    while true
-        if I_sort(I_sort_post_crack(i)-j)<=n_pecan_pre_crack
-            pre_crack_ind = I_sort(I_sort_post_crack(i)-j);
-            break
-        else
-            j = j+1;
-        end
-    end
-    
-    % get filename
-    pre_crack_file = fullfile(...
-        pre_crack_files(pre_crack_ind).folder,...
-        pre_crack_files(pre_crack_ind).name);
-    
-    [pecan_method_comp(i,2),~,~] = PHE(pre_crack_file,post_crack_file,...
-        'method','direct_area');
-end
+end % time_unix
--- pecan_method_comparison_plot.m
+++ pecan_method_comparison_plot.m
@@ -1,9 +1,62 @@
-binRange = 15:5:75;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% DDSL - Pecan Project
+% 
+% Compare direct area method, bounding box, and calibrated surface methods
+%
+% Author: Dani Agramonte
+% Last Updated: 04.28.22
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%binRange = 75:5:135;
+%% MATLAB initialization
+clear; % Clear variables
+clc;  % Clear command window.
+workspace;  % Make sure the workspace panel is showing.
 
-hcx = histcounts(pecan_method_comp(:,1),[binRange Inf]);
-hcy = histcounts(pecan_method_comp(:,2),[binRange Inf]);
+% load in method comparison data
+load('C:\Users\Dani\Documents\Pecan-Project-Image-Processing\Pecan_Calibration_Data\PMC_Data.mat','pecan_method_comp')
 
-figure
-bar(binRange,[hcx;hcy]')
+%% Plot
+
+% set binRange
+binRange = 78:2:114;
+
+% histcounts
+hcbb = histcounts(pecan_method_comp(:,1),[binRange Inf]);
+hcda = histcounts(pecan_method_comp(:,2),[binRange Inf]);
+hccs = histcounts(pecan_method_comp(:,3),[binRange Inf]);
+
+% fit burr distributions
+burr_bb = fitdist(pecan_method_comp(:,1),'burr');
+burr_da = fitdist(pecan_method_comp(:,2),'burr');
+burr_cs = fitdist(pecan_method_comp(:,3),'burr');
+
+% plot distributions with bar graph
+figure(1)
+bar(binRange,[hcbb;hcda;hccs]')
+
+% plot cdfs
+figure(2)
+% experimental CDFs
+cdfplot(pecan_method_comp(:,1))
+hold on
+cdfplot(pecan_method_comp(:,2))
+cdfplot(pecan_method_comp(:,3))
+
+% find mins and maxes for different data
+min_bb = min(pecan_method_comp(:,1));
+max_bb = max(pecan_method_comp(:,1));
+min_da = min(pecan_method_comp(:,2));
+max_da = max(pecan_method_comp(:,2));
+min_cs = min(pecan_method_comp(:,3));
+max_cs = max(pecan_method_comp(:,3));
+
+% find x values
+x_bb = linspace(min_bb,max_bb,1e5);
+x_da = linspace(min_da,max_da,1e5);
+x_cs = linspace(min_cs,max_cs,1e5);
+
+% plot cdfs
+plot(x_bb,cdf(burr_bb,x_bb))
+plot(x_da,cdf(burr_da,x_da))
+plot(x_cs,cdf(burr_cs,x_cs))
