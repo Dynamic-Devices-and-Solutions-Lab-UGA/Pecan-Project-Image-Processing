8694f73bb89f4862ad8ef8631a63cc3b952ab238
JosriqueJotaro <74071056+JosriqueJotaro@users.noreply.github.com>
2022-06-02 22:09:51 -0400

    Update ImpactorDesignModel.m

--- ImpactorDesignModel.m
+++ ImpactorDesignModel.m
@@ -8,3 +8,20 @@
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+% semi-major and semi-minor axes
+a = 0.940;
+b = 0.522;
+
+% parametric variable
+t = linspace(0,2*pi,10000);
+
+% ellipse
+x = a*cos(t);
+y = b*sin(t);
+
+% slope for line
+m = 
+
+plot(x,y)
+yline(0.75)
+yline(-0.75)
