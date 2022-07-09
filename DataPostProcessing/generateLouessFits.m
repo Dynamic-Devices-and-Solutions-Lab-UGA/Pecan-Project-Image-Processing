e2852edc1f57a4ab5995578fecd074e1bd0e7372
Dani Agramonte <74071056+JosriqueJotaro@users.noreply.github.com>
2022-07-09 14:27:10 -0600

    finish generating all lowess fit data

--- DataPostProcessing/generateLouessFits.m
+++ DataPostProcessing/generateLouessFits.m
@@ -34,103 +34,47 @@ combs = [...
     3 1;...
     2 2];
 
+coordSys = 'MH';
+
+opts1.type = 'P';
+opts1.coordSys = coordSys;
+
+opts2.type = 'S';
+opts2.coordSys = coordSys;
+
+
 for i = 1:size(combs,1)
     % get fits
-    [var_outIntegrity,PecanMeatIntegrity_sfit,gIntegrity] = getHSlice(angleFix(combs(i,1)),materialFix{combs(i,2)},pecan_data_struct,'P');
-    [var_outShellability,PecanShellability_sfit,gShellability] = getHSlice(angleFix(combs(i,1)),materialFix{combs(i,2)},pecan_data_struct,'S');
+    [var_outIntegrity,PecanMeatIntegrity_sfit,gIntegrity] = getHSlice(angleFix(combs(i,1)),materialFix{combs(i,2)},pecan_data_struct,opts1);
+    [var_outShellability,PecanShellability_sfit,gShellability] = getHSlice(angleFix(combs(i,1)),materialFix{combs(i,2)},pecan_data_struct,opts2);
 
     % generate names
-    namePMI = fullfile(projectPath,'DataPostProcessing','LowessFits',sprintf('PecanMeatIntegrityFit_%.f_%s.mat',angleFix(combs(i,1)),materialFix{combs(i,2)}));
-    nameShell = fullfile(projectPath,'DataPostProcessing','LowessFits',sprintf('ShellabilityFit_%.f_%s.mat',angleFix(combs(i,1)),materialFix{combs(i,2)}));
+    namePMI = fullfile(projectPath,'DataPostProcessing','LowessFits',sprintf([coordSys,'PecanMeatIntegrityFit_%.f_%s.mat'],angleFix(combs(i,1)),materialFix{combs(i,2)}));
+    nameShell = fullfile(projectPath,'DataPostProcessing','LowessFits',sprintf([coordSys,'ShellabilityFit_%.f_%s.mat'],angleFix(combs(i,1)),materialFix{combs(i,2)}));
 
     % save variables
     save(namePMI,'var_outIntegrity','PecanMeatIntegrity_sfit','gIntegrity')
     save(nameShell,'var_outShellability','PecanShellability_sfit','gShellability')
 end
 
-%{
-% data for opts = 'P'
-X1 = var_out1(:,1);
-Y1 = var_out1(:,2);
-Z1 = var_out1(:,3);
-w1 = var_out1(:,4);
-
-% data for opts = 'S' 
-X2 = var_out2(:,1);
-Y2 = var_out2(:,2);
-Z2 = var_out2(:,3);
-w2 = var_out2(:,4);
-
-h1 = figure;
-plot(PecanMeatIntegrity_sfit,[X1 Y1],Z1);
-
-% set background to white
-set(gcf,'color','white')
-
-% specify zlimits
-zlim([0 105])
-
-% specify colormap
-colormap(jet)
-
-% Label axes
-xlabel('Mass [kg]');
-ylabel('Height [m]');
-zlabel('Pecan Integrity, $\Psi [\%]$');
-
-% specify title
-% paramTitle = sprintf('Sampling of $V$-$E$ Parameter space: Material = %s,  $\\phi$  = %2d$^{\\circ}$',materialFix,angleFix);
-title(paramTitle)
-
-% turn grid on
-grid on
-
-h2 = figure;
-
-% plot surface
-plot(PecanShellability_sfit,[X2 Y2],Z2);
-
-% set background to white
-set(gcf,'color','white')
-
-% specify zlimits
-zlim([0 105])
-
-% specify colormap
-colormap(jet)
-
-% Label axes
-xlabel('Mass $[g]$');
-ylabel('Height $[cm]$');
-zlabel('Pecan Shellability, $\Xi [\%]$');
-
-% turn grid on
-grid on
-
-% calculate upper and lower bounds for m and
-lb = [min(X1), min(Y1)];
-ub = [max(X1), max(Y1)];
-
-% home folder
-folder_loc = fullfile(projectPath,'Pecan_Surface_Fits');
-
-% file name
-% name = sprintf('PecanSurfaceFits-Angle.%d-Material.%s.mat',angleFix,materialFix);
-
-% save data
-% save(fullfile(folder_loc,name),'PecanMeatIntegrity_sfit','PecanShellability_sfit','lb','ub')
-%}
-
 clear;
 
-
+%---- END MAIN SCRIPT ----%
 
 function [var,fitresult,gof] = getHSlice(angleFix,materialFix,pecan_data_struct,opts)
 
-if strcmp(opts,'S')
-    param = 0;
-elseif strcmp(opts,'P')
-    param = 1;
+if strcmp(opts.type,'S')
+    param.type = 0;
+elseif strcmp(opts.type,'P')
+    param.type = 1;
+else
+    error('Invalid value for ''opts''')
+end
+
+if strcmp(opts.coordSys,'MH')
+    param.coordSys = 0;
+elseif strcmp(opts.coordSys,'VE')
+    param.coordSys = 1;
 else
     error('Invalid value for ''opts''')
 end
@@ -182,7 +126,7 @@ H(((ind_stop+1):end)) = [];
 P(((ind_stop+1):end)) = [];
 S(((ind_stop+1):end)) = [];
 
-if param
+if param.type
     
     % delete zero values
     H(P == 0) = [];
@@ -198,28 +142,34 @@ else
 end
 
 % assign matrices to their proper variables
-H = matOut(:,1);
-M = matOut(:,2);
+var1 = matOut(:,1); % V normally
+var2 = matOut(:,2); % M normally
+
+if param.coordSys
+    [var1,var2] = coordTrans(var1,var2);
+    % var1 = V
+    % var2 = E
+end
 
 % find weights by taking into consideration number of tests
 w = groupcounts(uidx);
 
-if param
+if param.type
     % call LowessFit to get actual results
-    [fitresult, gof] = LouessFit(M,H,avgP,w,'lowess');
+    [fitresult, gof] = LouessFit(var2,var1,avgP,w,'lowess',param);
     
     % assign vars out
-    var = [M,H,avgP,w];
+    var = [var2,var1,avgP,w];
 else
     % call LowessFit to get actual results
-    [fitresult, gof] = LouessFit(M,H,avgS,w,'loess');
+    [fitresult, gof] = LouessFit(var2,var1,avgS,w,'loess',param);
     
     % assign vars out
-    var = [M,H,avgS,w];
+    var = [var2,var1,avgS,w];
 end
 end
 
-function [fitresult, gof] = LouessFit(X1,X2,X3,w,opts)
+function [fitresult, gof] = LouessFit(X1,X2,X3,w,opts,param)
 
 if ~(strcmp(opts,'loess')||strcmp(opts,'lowess'))
     error('Invalid value for ''opts''')
@@ -232,8 +182,12 @@ end
 ft = fittype(opts);
 opts = fitoptions('Method','LowessFit');
 opts.Normalize = 'on';
-opts.Robust = 'Bisquare';
-opts.Span = 0.75;
+if ~param.coordSys
+    opts.Robust = 'Bisquare';
+    opts.Span = 0.75;
+else
+    opts.Span = 0.3;
+end
 opts.Weights = weights;
 
 % Fit model to data.
@@ -248,99 +202,4 @@ g = 9.8;
 V = (2*g*(H/100)).^0.5;
 E = g*(M/1000).*(H/100);
 
-end
-
-function [var,fitresult, gof] = getVSlice(coordFix,pecan_data_struct)
-
-switch coordFix
-    case {'v','velocity'}
-        flag = 0;
-    case {'e','energy'}
-        flag = 1;
-    otherwise
-        error('invalid value for coordFix')
-end
-
-% set fixed material - unfortunately we don't have enough data 
-% to properly compare durable resin and steel
-materialFix = 'Steel';
-
-% initialize variables
-M = zeros(5e4,1);
-H = zeros(5e4,1);
-P = zeros(5e4,1);
-A = zeros(5e4,1);
-S = zeros(5e4,1);
-
-% possible set of angles
-angleSet = [15,30,45];
-
-% extract from datastructure
-start_shift = 0;
-for k = 1:3
-    angleLoc = angleSet(k);
-    for i = 1:numel(pecan_data_struct)
-        if (pecan_data_struct(i).metadata.Angle == angleLoc) && ...
-                (strcmp(pecan_data_struct(i).metadata.Material,materialFix))
-            for j = 1:numel(pecan_data_struct(i).test)
-
-                % calculate start and stop indices
-                ind_start = start_shift+2*(j-1)+1;
-                ind_stop = start_shift+2*(j-1)+2;
-
-                % set values
-                M(ind_start:ind_stop) = pecan_data_struct(i).metadata.Mass;
-                H(ind_start:ind_stop) = pecan_data_struct(i).metadata.Height;
-                P(ind_start:ind_stop) = pecan_data_struct(i).test(j).post_crack_data.half(1:2).perc;
-                S(ind_start:ind_stop) = 100*ceil(pecan_data_struct(i).test(j).post_crack_data.half(1:2).perc/100);
-                A(ind_start:ind_stop) = pecan_data_struct(i).metadata.Angle;
-            end
-
-            start_shift = start_shift+2*numel(pecan_data_struct(i).test);
-        end
-    end
-end
-
-% remove zero padding
-M(((ind_stop+1):end)) = [];
-H(((ind_stop+1):end)) = [];
-P(((ind_stop+1):end)) = [];
-A(((ind_stop+1):end)) = [];
-S(((ind_stop+1):end)) = [];
-
-% tranformed coordate system
-[V,E] = coordTrans(M,H);
-
-% find average P values
-[EA_uniq, ~, uidxE] = unique([E(:), A(:)], 'rows');
-avgPE = accumarray(uidxE, P(:), [], @mean);
-
-E_uniq = EA_uniq(:,1);
-AE_uniq = EA_uniq(:,2);
-
-% find weights by taking into consideration number of tests
-wE = groupcounts(uidx);
-
-% find average P values
-[VA_uniq, ~, uidxV] = unique([V(:), A(:)], 'rows');
-avgPV = accumarray(uidxV, P(:), [], @mean);
-
-V_uniq = VA_uniq(:,1);
-AV_uniq = VA_uniq(:,2);
-
-% find weights by taking into consideration number of tests
-wV = groupcounts(uidxV);
-
-if flag
-    % call LowessFit to get actual results
-    [fitresult, gof] = LouessFit(E_uniq,AE_uniq,avgPE,wE);
-    
-    % assign vars out
-    var = [E_uniq,AE_uniq,avgPE];
-else
-    % call LowessFit to get actual results
-    [fitresult, gof] = LouessFit(V_uniq,AV_uniq,avgPV,wV);
-    % assign vars out
-    var = [V_uniq,AV_uniq,avgPV];
-end
 end
